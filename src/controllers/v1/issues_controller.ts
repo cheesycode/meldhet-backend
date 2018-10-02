caa0e4f595bad156e2c28a78149f330314e64839
Michel Megens <dev@bietje.net>
2018-10-02 12:36:53 +0200

    Save issue's using KV, instead of an object with KV's.
    Signed-off-by: Michel Megens <dev@bietje.net>

--- src/controllers/v1/issues_controller.ts
+++ src/controllers/v1/issues_controller.ts
@@ -2,8 +2,6 @@
 "use strict";
 
 import { Response, Request, NextFunction } from "express";
-import { Issue } from "../../model/issue";
-import app from '../../app';
 import firebase from 'firebase';
 
 export let createIssue = (req: Request, res: Response, next: NextFunction) => {
@@ -11,16 +9,12 @@ export let createIssue = (req: Request, res: Response, next: NextFunction) => {
 	console.log('Longitude: ' + req.body.lon);
 	console.log(req.body);
 
-	const obj = {
+	firebase.database().ref('issues/' + req.body.id).set({
 		lat: req.body.lat,
 		lon: req.body.lon,
 		acc: req.body.acc,
 		image: req.body.acc,
 		tag: req.body.tag
-	}
-
-	firebase.database().ref('issues/' + req.body.id).set({
-		obj
 	});
 
 	res.json({ok: true})
